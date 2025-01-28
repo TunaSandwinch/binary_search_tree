357da8409eb775116d08b9674534059b55bb5adf
TunaSandwinch <winch.mascarenas@gmail.com>
2025-01-28 19:57:01 +0800

    feat: add instance variables

---
+++ lib/node.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+# node class
+class Node
+  def initialize(node_value, left_child, right_child)
+    @node_value = node_value
+    @left_child = left_child
+    @right_child = right_child
+  end
+end

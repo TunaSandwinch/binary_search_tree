989256125a993a02904a1a770773ecaf0ce5f966
TunaSandwinch <winch.mascarenas@gmail.com>
2025-02-02 16:58:30 +0800

    feat: add insert method to insert new value to the tree

---
+++ lib/tree.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+require_relative 'node'
+require_relative 'sort'
+# tree class
+class Tree
+  attr_reader :root
+
+  def initialize(array)
+    @root = build_tree(sort(array.uniq))
+  end
+
+  def pretty_print(node = @root, prefix = '', is_left = true)
+    pretty_print(node.right_child, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right_child
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.value}"
+    pretty_print(node.left_child, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left_child
+  end
+
+  def insert(value, current_node = root)
+    path = current_node.value >= value ? current_node.left_child : current_node.right_child
+    if path.nil?
+      return current_node.value >= value ? current_node.left_child = Node.new(value, nil,
+                                                                              nil) : current_node.right_child = Node.new(
+                                                                                value, nil, nil
+                                                                              )
+    end
+    return if current_node.value == value
+
+    insert(value, path)
+  end
+
+  private
+
+  def build_tree(array) # rubocop:disable Metrics/AbcSize
+    mid = array.length / 2
+    left = array[0..mid - 1]
+    right = array[(mid + 1)..array.length]
+    return Node.new(array[mid], nil, nil) if array.length == 1
+
+    return Node.new(array[mid], build_tree(left), nil) if array.length == 2
+
+    Node.new(array[mid], build_tree(left), build_tree(right))
+  end
+end
+
+array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
+
+test = Tree.new(array)
+test.insert(500)
+test.pretty_print

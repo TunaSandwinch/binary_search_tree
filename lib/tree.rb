2fb060b8a926db3754d519481a4268df7423effc
TunaSandwinch <winch.mascarenas@gmail.com>
2025-02-10 12:07:27 +0800

    feat: delete root node of the tree

--- lib/tree.rb
+++ lib/tree.rb
@@ -17,7 +17,7 @@ class Tree
   end
 
   def insert(value, current_node = root)
-    path = current_node.value >= value ? current_node.left_child : current_node.right_child
+    path = current_node.value > value ? current_node.left_child : current_node.right_child
     if path.nil?
       return current_node.value >= value ? current_node.left_child = Node.new(value, nil,
                                                                               nil) : current_node.right_child = Node.new(
@@ -29,7 +29,46 @@ class Tree
     insert(value, path)
   end
 
+  def delete(value, current_node = root)
+    path = current_node.value > value ? current_node.left_child : current_node.right_child
+    return nil if path.nil?
+    return del_root if current_node.value == value
+    return 'left' if current_node.left_child.value == value
+    return 'right' if current_node.right_child.value == value
+
+    delete(value, path)
+  end
+
   private
+
+  def del_root # rubocop:disable Metrics/AbcSize
+    if root.right_child.left_child.nil?
+      successor = root.right_child
+      @root = (successor.left_child = root.left_child)
+    else
+      successor = lowest_node(root.right_child)
+      successor.left_child = root.left_child
+      prev_node(successor).left_child = successor.right_child # current child of successor
+      @root = successor.right_child = root.right_child # assign new child to the successor
+      @root = successor
+    end
+  end
+
+  def prev_node(address, current_node = root)
+    path = current_node.value > address.value ? current_node.left_child : current_node.right_child
+    return nil if address.value == root.value
+    return current_node if path.value == address.value
+
+    # stack level too deeep
+    # find a way to play around path
+    prev_node(address, path)
+  end
+
+  def lowest_node(address = root)
+    return address if address.left_child.nil?
+
+    lowest_node(address.left_child)
+  end
 
   def build_tree(array) # rubocop:disable Metrics/AbcSize
     mid = array.length / 2
@@ -47,4 +86,7 @@ array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
 
 test = Tree.new(array)
 test.insert(500)
+test.pretty_print
+test.delete(8)
+puts ' '
 test.pretty_print

32b7410fb169e702e4994b8f0b575777e42703df
TunaSandwinch <winch.mascarenas@gmail.com>
2025-02-11 21:03:23 +0800

    feat: delete a node with a given value

--- lib/tree.rb
+++ lib/tree.rb
@@ -32,18 +32,35 @@ class Tree
   end
 
   def delete(value, current_node = root)
-    path = current_node.value > value ? current_node.left_child : current_node.right_child
-    return nil if path.nil?
-    return del_root if root.value == value
+    next_node = current_node.value >= value ? current_node.left_child : current_node.right_child
+    return nil if next_node.nil?
+    return del_node(current_node) if value == root.value
+    return del_node(next_node, current_node) if next_node.value == value
 
-    return del_node(path, current_node) if path.value == value
-
-    # play around path
-    # del node goes on infinite loop
-    delete(value, path)
+    delete(value, next_node)
   end
 
   private
+
+  def del_node(address, prev_node = nil) # rubocop:disable Metrics/AbcSize,Metrics/MethodLength
+    if address.left_child.nil? && address.right_child.nil?
+      prev_node.value > address.value ? prev_node.left_child = nil : prev_node.right_child = nil
+    elsif address.left_child.nil?
+      address.value = address.right_child.value
+      address.right_child = address.right_child.right_child
+      address.left_child = address.right_child.left_child
+    else
+      successor = lowest_node(address.right_child)
+      delete(successor.value)
+      address.value = successor.value
+    end
+  end
+
+  def lowest_node(address)
+    return address if address.left_child.nil?
+
+    lowest_node(address.left_child)
+  end
 
   def build_tree(array) # rubocop:disable Metrics/AbcSize
     mid = array.length / 2
@@ -60,6 +77,14 @@ end
 array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
 
 test = Tree.new(array)
+puts 'original tree >>>>>>>>>>>>>'
+puts ''
+test.print_bst
+puts 'insert 24 to the tree >>>>>>>>>>>'
+puts ''
 test.insert(24)
-test.delete(67)
+test.print_bst
+puts 'delete 64 from the tree >>>>>>>>>>'
+puts ''
+test.delete(64)
 test.print_bst
